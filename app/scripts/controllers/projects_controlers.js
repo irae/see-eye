2889d19bdca9b95f9adf9126d075c0ab6fbb926e
Irae <irae@irae.pro.br>
2013-09-20 14:20:30 -0700

    fix excess interval; remove logs

--- app/scripts/controllers/projects_controlers.js
+++ app/scripts/controllers/projects_controlers.js
@@ -7,15 +7,13 @@ App.ProjectsController = Ember.ArrayController.extend({
       project.get('builds').forEach(function(build) {
         var result = build.get('result');
         if(!result || result.length < 1) {
-          if(!build._refreshInterval) {
-            console.log('setInterval project', project.get('name'), ' build ', build.get('number'));
+          if(!build._refreshInterval && build.get('number')) {
             build._refreshInterval = setInterval(function(){
               build.reload();
             }, 5000);
           }
         } else {
           if(build._refreshInterval) {
-            console.log('CLEAR project', project.get('name'), ' build ', build.get('number'));
             clearInterval(build._refreshInterval);
             delete build._refreshInterval;
           }

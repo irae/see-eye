df14f6f9a49bf700f0e6e717afdcb2995c65a916
Irae <irae@irae.pro.br>
2013-09-20 11:18:04 -0700

    sort of authentication, shameless hack

--- app/scripts/store.js
+++ app/scripts/store.js
@@ -3,9 +3,7 @@ Ember.$.ajaxSetup({
   type: "jsonp",
   jsonp: "jsonp",
   error: function(xhr, status, error) {
-    var errorInStrings = (status && /error/i.test(status) && error && /Forbidden/i.test(error));
-    var errorInXHR = (xhr && xhr.status && xhr.status === 403);
-    if(errorInXHR || errorInStrings) {
+    if(status === "parsererror") {
       document.location.href = "https://gh.bouncer.login.yahoo.com/login?url=" + encodeURIComponent(document.location.href);
     }
   }
